debb3c661a971e4619621e1e613c0418e6ff2303
Michelle Wang <tianw0420@gmail.com>
2021-12-24 08:04:36 -0800

    added button animation and reset and fixed functionality

--- script.js
+++ script.js
@@ -21,54 +21,68 @@ rock.addEventListener('click', () => {
         remainingRounds--; 
         roundsLeft.textContent = remainingRounds; 
     
-    let cPlay = computerPlay(); 
-    if (cPlay == 'scissors') { 
-        playerScore.textContent = pScore++; 
-    } else if (cPlay == 'paper'){
-        computerScore.textContent = cScore++;
-    } else {  }
-    } else {
-        roundsLeft.textContent = 0; 
+        playerPlay(computerPlay(), 0); 
+
+    } else if (remainingRounds == 1){
+        remainingRounds--; 
+        roundsLeft.textContent = remainingRounds; 
+    
+        playerPlay(computerPlay(), 0); 
         endGame(); 
-    } ; 
+    } else {
+
+    }
 
    
 }); 
-paper.addEventListener('click', () =>{ if (remainingRounds > 1){
-    remainingRounds--; 
-    roundsLeft.textContent = remainingRounds; 
+paper.addEventListener('click', () =>{ 
+    if (remainingRounds > 1){
+        remainingRounds--; 
+        roundsLeft.textContent = remainingRounds; 
 
-    let cPlay = computerPlay(); 
-    if (cPlay == 'rock') { 
-        playerScore.textContent = pScore++; 
-    } else if (cPlay == 'scissors'){
-        computerScore.textContent = cScore++;
-    } else {  }
-    } else {
-        roundsLeft.textContent = 0; 
+        playerPlay(computerPlay(), 1); 
+    } else if (remainingRounds == 1){
+        remainingRounds--; 
+        roundsLeft.textContent = remainingRounds; 
+    
+        playerPlay(computerPlay(), 1); 
         endGame(); 
-        } 
+    } else {
+        
+    } 
 }); 
 
-scissors.addEventListener('click', () => { if (remainingRounds > 1){
-    remainingRounds--; 
-    roundsLeft.textContent = remainingRounds; 
+scissors.addEventListener('click', () => { 
+    if (remainingRounds > 1){
+        remainingRounds--; 
+        roundsLeft.textContent = remainingRounds; 
 
-    let cPlay = computerPlay(); 
-    if (cPlay == 'paper') { 
-        playerScore.textContent = pScore++; 
-    } else if (cPlay == 'rock'){
-        computerScore.textContent = cScore++;
-    } else {  }
-    } else {
-        roundsLeft.textContent = 0; 
+        playerPlay(computerPlay(), 2); 
+
+    } else if (remainingRounds == 1){
+        remainingRounds--; 
+        roundsLeft.textContent = remainingRounds; 
+    
+        playerPlay(computerPlay(), 2); 
         endGame(); 
-        } 
+    } else {
+        
+    }
 });
 
 function computerPlay() {
     let index = Math.floor(Math.random()*3); 
-    return choice[index]; 
+    console.log(choice[index]); 
+    console.log('p score: ' + pScore); 
+    console.log('c score: ' + cScore); 
+    return index; 
+}
+function playerPlay(cIndex, pIndex) {
+    if ((pIndex -1) == cIndex || (pIndex + 2) == cIndex){
+        playerScore.textContent = ++pScore;
+    } else if (pIndex != cIndex){
+        computerScore.textContent = ++cScore;
+    } else {console.log('draw'); }
 }
 
 function endGame(){
@@ -76,14 +90,33 @@ function endGame(){
     if (remainingRounds < -1){
 
     } else {
-    let outcome = document.createElement('div');
-    
-    if (pScore > cScore){
-        outcome.textContent = 'player wins!'; 
-    } else if (pScore < cScore){
-        outcome.textContent = 'computer wins!'; 
-    } else {
-        outcome.textContent = 'it was a draw!'; 
+        let outcome = document.createElement('div');
+        
+        if (pScore > cScore){
+            outcome.textContent = 'player wins!'; 
+        } else if (pScore < cScore){
+            outcome.textContent = 'computer wins!'; 
+        } else {
+            outcome.textContent = 'it was a draw!'; 
+        }
+        container.appendChild(outcome); 
+
+        let resetButton = document.createElement('button'); 
+        resetButton.textContent = 'reset game'; 
+        resetButton.addEventListener('click', () => {
+            reset(); 
+            outcome.remove(); 
+            resetButton.remove(); 
+        }); 
+        container.appendChild(resetButton); 
     }
-    container.appendChild(outcome); }
+}
+
+function reset() {
+    pScore = 0; 
+    cScore = 0; 
+    remainingRounds = 5; 
+    playerScore.textContent = pScore; 
+    computerScore.textContent = cScore; 
+    roundsLeft.textContent = remainingRounds; 
 }
